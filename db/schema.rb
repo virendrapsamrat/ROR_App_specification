93bc0b110c4bc159b3050760febde02cdb6243f2
Virendra pratap <arvindkushwah9@gmail.com>
2019-04-18 19:08:55 +0530

    final sub

--- db/schema.rb
+++ db/schema.rb
@@ -96,9 +96,18 @@ ActiveRecord::Schema.define(version: 2019_04_18_113256) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
-
-# Could not dump table "users" because of following StandardError
-#   Unknown type 'file' for column 'image_id'
+  
+  create_table "users", force: :cascade do |t|
+      t.string "email", default: "", null: false
+      t.string "encrypted_password", default: "", null: false
+      t.string "reset_password_token"
+      t.datetime "reset_password_sent_at"
+      t.datetime "remember_created_at"
+      t.datetime "created_at", null: false
+      t.datetime "updated_at", null: false
+      t.index ["email"], name: "index_users_on_email", unique: true
+      t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+    end
 
   create_table "your_comments", force: :cascade do |t|
     t.string "commenter"
